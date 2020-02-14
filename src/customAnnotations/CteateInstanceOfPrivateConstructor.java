0a424b1161d1be60f90c6be31bc9b1186dc469cb
Oleg Shapoval <Olegx4@ukr.net>
2020-02-14 02:43:37 +0200

    Changed the title of the annotation to a clearer one

--- src/customAnnotations/CteateInstanceOfPrivateConstructor.java
+++ src/customAnnotations/CteateInstanceOfPrivateConstructor.java
@@ -8,6 +8,6 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.CONSTRUCTOR)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface GetPrivateConstructor {
+public @interface CteateInstanceOfPrivateConstructor {
 }
 
